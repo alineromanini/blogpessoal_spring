5a6e4bfc264e31fb405ac6baa7f73468cceb2960
Aline <sromanini.aline@gmail.com>
2025-10-14 11:46:40 -0300

    :sparkles: Finalização do CRUD do Recurso Portagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -24,7 +35,50 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 		
-		//SELECT * FROM tb_postagens;
+		//findAll é o equivalente a SELECT * FROM tb_postagens;
+		
+	}
+	
+	@GetMapping("/{id}") //para procurar a variavel no endereço, chamada de variavel de caminho
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){ //o parametro pega o id do endereço e joga no método
+		return postagemRepository.findById(id) //nao pode retornar ResponseEntity pq ñ pode retornar valor nulo
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+	//findById é o equivalente a SELECT * FROM tb_postagens WHERE id = ?;
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getAllByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){ //Valid valida algumas regras q estão na Model; RequestBody pega postagem dentro do Body
+		postagem.setId(null); //seta o id como nulo para não criar nenhum ID aqui
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping 
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+			.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem)))
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		
+		Optional<Postagem> postagem = postagemRepository.findById(id); //verifica se a postagem existe
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND); //se nao encontrar a postagem vai mostrar a msg NOT FOUND
+		
+		postagemRepository.deleteById(id); //se encontrar vai deletar
 		
 	}
 	
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -32,33 +32,41 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
 
-	public Long geId() {
+	public Long getId() {
 		return id;
 	}
-	
-	public void setId(long id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+	
+
+	
 	
 	
 }
